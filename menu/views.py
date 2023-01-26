8f0734bd62b47b4718051864a784efb678c05418
R1ck_S4ncherz <121076119+naveen30112002@users.noreply.github.com>
2023-01-26 14:38:52 +0530

    added cors

--- menu/views.py
+++ menu/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+import json
 
 #rest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import food_details
+from django.core import serializers
 
 
 
 @api_view(["GET"])
 def list_menu_items(request):
-    return Response("hello")
+    return_data=json.loads(serializers.serialize("json",food_details.objects.all()))
+    
+    return Response(return_data)
 
